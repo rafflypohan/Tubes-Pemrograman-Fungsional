c02ce499bb638f9bede7b643b5bdbd44c0fcc948
Raffly <rafflypohan17@gmail.com>
2020-12-27 01:25:57 +0800

    hampir fix

--- Fuzzy.py
+++ Fuzzy.py
@@ -1,46 +1,27 @@
-import multiprocessing as mp
-import itertools
+import os
+import sys
+import csv
+from itertools import product
+from multiprocessing import Process, Manager
+import pandas
 
 
 def linear_turun(a, b, x):
-    if x <= a:
-        derajat_keanggotaan = 1
-    elif a <= x <= b:
-        derajat_keanggotaan = (b - x) / (b - a)
-    else:
-        derajat_keanggotaan = 0
+    derajat_keanggotaan = (1 if x <= a else (b - x) / (b - a) if a <= x <= b else 0)
     return derajat_keanggotaan
 
 
 def linear_naik(a, b, x):
-    if x <= a:
-        derajat_keanggotaan = 0
-    elif a <= x <= b:
-        derajat_keanggotaan = (x - a) / (b - a)
-    else:
-        derajat_keanggotaan = 1
+    derajat_keanggotaan = (0 if x <= a else (x - a) / (b - a) if a <= x <= b else 1)
     return derajat_keanggotaan
 
 
 def segitiga(a, b, c, x):
-    if a <= x <= b:
-        derajat_keanggotaan = (x - a) / (b - a)
-    elif b <= x <= c:
-        derajat_keanggotaan = (c - x) / (c - b)
-    else:
-        derajat_keanggotaan = 0
+    derajat_keanggotaan = ((x - a) / (b - a) if a <= x <= b else (c - x) / (c - b) if b <= x <= c else 0)
     return derajat_keanggotaan
 
 
-def z_dapat(alpha, x):
-    return (80 - 40) * alpha[x] + 40
-
-
-def z_tidak_dapat(alpha, x):
-    return 80 - (alpha[x]) * (80 - 40)
-
-
-def fungsi_keanggotaan(ipk, penghasilan, jarak):
+def fungsi_keanggotaan(ipk, penghasilan, jarak, res):
     ipk_rendah = lambda x: linear_turun(1.5, 2.5, x)
     ipk_sedang = lambda x: segitiga(1.5, 2.5, 3.5, x)
     ipk_tinggi = lambda x: linear_naik(2.5, 4, x)
@@ -58,21 +39,22 @@ def fungsi_keanggotaan(ipk, penghasilan, jarak):
                          penghasilan_tinggi(penghasilan)]
     nilai_jarak = [jarak_dekat(jarak), jarak_sedang(jarak), jarak_jauh(jarak)]
 
-    inferensi(nilai_ipk, nilai_penghasilan, nilai_jarak)
+    inferensi(nilai_ipk, nilai_penghasilan, nilai_jarak, res)
 
 
-def inferensi(nilai_ipk, nilai_penghasilan, nilai_jarak):
+def inferensi(nilai_ipk, nilai_penghasilan, nilai_jarak, res):
     x = 0
     alpha = []
     z = []
     ls = []
-
-    for i, j, k in itertools.product(nilai_ipk, nilai_penghasilan, nilai_jarak):
-        if (i > 0) and (j > 0) and (k > 0):
+    z_dapat = lambda alpha, x: (80 - 40) * alpha[x] + 40
+    z_tidak_dapat = lambda alpha, x: 80 - (alpha[x]) * (80 - 40)
+    for i, j, k in product(nilai_ipk, nilai_penghasilan, nilai_jarak):
+        if (i and j and k) > 0:
             ls.clear()
             ls.extend([i, j, k])
             alpha.append(min(ls))
-            # print(alpha)
+
             if nilai_ipk.index(i) == 2 and nilai_penghasilan.index(j) == 1 and nilai_jarak.index(k) == 2:
                 z.append(z_dapat(alpha, x))
             elif nilai_ipk.index(i) == 2 and nilai_penghasilan.index(j) == 0 and nilai_jarak.index(k) == 2:
@@ -90,18 +72,73 @@ def inferensi(nilai_ipk, nilai_penghasilan, nilai_jarak):
             print(f"IF IPK = {i} AND Penghasilan = {j} AND Jarak = {k} THEN z = {z[x]} alpha = {alpha[x]} x = {x}")
             x += 1
 
-    defuzzifikasi(alpha, z)
+    defuzzifikasi(alpha, z, res)
 
 
-def defuzzifikasi(alpha, z):
+def defuzzifikasi(alpha, z, res):
     # rumus centroid method
     # ((alpha1 * z1) + (alpha2 * z2) + ...) / (alpha1 + alpha2 + ...)
     jum = (a * b for a, b in zip(alpha, z))
     defuzi = sum(jum) / sum(alpha)
-    print("Defuzzifikasi = " + "{:.2f}".format(defuzi) + "%")
+    res.value = round(defuzi, 2)
+    print("Hasil Defuzzifikasi = ", round(defuzi, 2), "%")
+
+
+def read_csv(filename):
+    print(pandas.read_csv(filename))
+
+
+def write_csv(filename, nama, ipk, penghasilan, jarak, peluang):
+    rows = ["Nama", "IPK", "Penghasilan", "Jarak", "Peluang (%)"]
+    if os.path.isfile(filename):
+        with open(filename, 'a', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=rows)
+            writer.writerow(
+                {"Nama": nama, "IPK": ipk, "Penghasilan": penghasilan, "Jarak": jarak, "Peluang (%)": peluang})
+    else:
+        with open(filename, 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=rows)
+            writer.writeheader()
+            writer.writerow(
+                {"Nama": nama, "IPK": ipk, "Penghasilan": penghasilan, "Jarak": jarak, "Peluang (%)": peluang})
+
+
+def menu(filename):
+    n = int(input("1. Tambah Data\n2. Tampilkan Data\n3. Keluar\nPilih Menu : "))
+    if n == 1:
+        main(filename)
+    elif n == 2:
+        read_csv(filename)
+        kembali = input("Kembali ke menu utama (y/n) : ")
+        (menu(filename) if kembali == "y" else sys.exit("Terima Kasih"))
+    elif n == 3:
+        sys.exit("Terima Kasih")
+    else:
+        print("Masukkan pilihan yang benar")
+        menu(filename)
+
+
+def main(filename):
+    manager = Manager()
+    res = manager.Value('d', 0)
+
+    nama = input("Masukkan Nama: ")
+    ipk = float(input("Masukkan IPK saat ini: "))
+    penghasilan = int(input("Masukkan Penghasilan Orang Tua : "))
+    jarak = float(input("Masukkan Jarak dari rumah anda ke Kampus/Sekolah (Km): "))
+
+    p = Process(target=fungsi_keanggotaan, args=(ipk, penghasilan, jarak, res,))
+    p.start()
+    p.join()
+
+    write_csv(filename, nama, ipk, penghasilan, jarak, res.value)
+
+    repeat = input("Apakah anda ingin menginput data lagi?(y/n) : ")
+    while repeat == "y":
+        main(filename)
+    menu(filename)
 
 
 if __name__ == '__main__':
-    p = mp.Process(target=fungsi_keanggotaan, args=(3.3, 50000, 60))
-    p.start()
-    p.join()
+    filename = 'tes.csv'
+    menu(filename)
